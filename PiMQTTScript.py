d0c3cab4ac6d4690cb3ec2c61a06a5b6ae0282fe
Aaron <107771768+a2ronprice@users.noreply.github.com>
2023-01-28 11:20:22 -0500

    Add files via upload

---
+++ PiMQTTScript.py
@@ -0,0 +1,49 @@
+import time
+import paho.mqtt.client as mqtt
+import serial
+
+clientName = "PiBot"
+serverAddress = "10.194.2.2"
+mqttClient = mqtt.Client(clientName)
+didPrintSubscribeMessage = False
+
+
+def connectionStatus(client, userdata, flags, rc):
+    global didPrintSubscribeMessage
+    if not didPrintSubscribeMessage:
+        didPrintSubscribeMessage = True
+        print("subscribing")
+        mqttClient.subscribe("pibot/move")
+        print("subscribed")
+
+
+def messageDecoder(client, userdata, msg):
+    message = msg.payload.decode(encoding='UTF-8')
+
+    if message == "forward":
+        print("^^^ moving forward! ^^^")
+    elif message == "stop":
+        print("!!! stopping!")
+    elif message == "backward":
+        print("\/ backward \/")
+    elif message == "left":
+        print("<- left")
+    elif message == "right":
+        print("-> right")
+    elif message == "drop":
+        print("drop")
+        ser.write("drop")
+    else:
+        print("message not found")
+
+
+# Set up calling functions to mqttClient
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+mqttClient.on_connect = connectionStatus
+mqttClient.on_message = messageDecoder
+ser.reset_input_buffer()
+
+# Connect to the MQTT server & loop forever.
+# CTRL-C will stop the program from running.
+mqttClient.connect(serverAddress)
+mqttClient.loop_forever()
