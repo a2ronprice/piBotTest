39a7eb5f108bfad1549c27ed65193bcd0d247327
Aaron <107771768+a2ronprice@users.noreply.github.com>
2023-01-28 11:34:22 -0500

    Add files via upload

--- PiMQTTScript.py
+++ PiMQTTScript.py
@@ -32,7 +32,7 @@ def messageDecoder(client, userdata, msg):
         print("-> right")
     elif message == "drop":
         print("drop")
-        ser.write(b"drop\n")
+        ser.write(b"drop")
     else:
         print("message not found")
 
